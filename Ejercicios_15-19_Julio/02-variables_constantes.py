f4cd7ee2178e64a8ae841b3c50d07884f809e84d
Gustavo <greynaga@outlook.com>
2024-07-18 11:56:27 -0700

    se agregan ejercicios 02 y 03

---
+++ Ejercicios_15-19_Julio/02-variables_constantes.py
@@ -0,0 +1,19 @@
+"""
+    Declarar Variables y Constantes en Python
+    link: https://www.python.org/
+    Creado por: Gustavo Reynaga
+    Julio del 2024
+"""
+
+# Variables ejemplo
+nombre = "Gustavo"
+
+# Constantes
+"""
+    Nota: En Python a diferencia de otros lenguajes
+    una constante puede ser modificada, por lo que
+    realmente es una variable mas, solo que en 
+    mayusculas.
+"""
+VALOR_PI = 3.141592
+
---
+++ Ejercicios_15-19_Julio/03-tipo_datos_primitivos.py
@@ -0,0 +1,43 @@
+"""
+    Declarar Variables con todos los
+    tipos de datos Primitvos en Python
+    link: https://www.python.org/
+    Creado por: Gustavo Reynaga
+    Julio del 2024
+"""
+"""
+    Python soporta los siguientes tipos
+    de datos primitivos
+    Numericos
+        int
+        float
+        complex
+    Booleanos
+    Cadena
+    None
+"""
+# Variables de tipo "int" (Entero)
+edad = 57
+temp_cels = -10
+
+# Variables de tipo float (Punto flotante)
+tipo_cambio_dollar = 18.45
+promedio_calif = 8.45
+
+# Variables de tipo complex
+numero1_complejo = 4 + 7j
+numero2_complejo = 3 + 5j
+
+# Variables de tipo Boolean
+pizza = True
+tacos = False
+
+# Variables de tipo Cadena (String)
+nombre = "Gustavo Salvador"
+calle = "Andador Playa del caimanero"
+
+# Variables de tipo None (nulo)
+nombre = None
+trabajo1 = None
+
+
