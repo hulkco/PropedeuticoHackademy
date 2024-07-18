788f19747287b4d10e5728fffcd70ffd04290eeb
Gustavo <greynaga@outlook.com>
2024-07-17 21:39:20 -0700

    Agregando comentarios

---
+++ Ejercicios_15-19_Julio/01-comentarios.py
@@ -0,0 +1,12 @@
+"""
+    Agregar comentarios en Python
+    link: https://www.python.org/
+    Creado por: Gustavo Reynaga
+    Julio del 2024
+"""
+
+# Agregando comentario usando # (hashtag)
+
+"""
+    Agregando comentario usando tres comillas dobles 
+"""
