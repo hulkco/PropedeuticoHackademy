d2af6692470c8d9e4a21a4d61e0262eb62c99f28
Gustavo <greynaga@outlook.com>
2024-07-18 20:03:12 -0700

    Correcion ejercicio 5

--- Ejercicios_15-19_Julio/05-operadores_aritmeticos_python.py
+++ Ejercicios_15-19_Julio/05-operadores_aritmeticos_python.py
@@ -9,13 +9,16 @@
     Operadores Aritméticos en Python        
 """
 # Aritméticos
-suma = 12 + 4
-resta = 12 - 4
-multiplicacion = 12 * 4
-division = 12 / 5
-modulo = 18 % 3
-potencia = 12 ** 4
-div_entero = 12 // 5
+numero_01 = 15
+numero_02 = 4
+
+suma = numero_01 + numero_02
+resta = numero_01 - numero_02
+multiplicacion = numero_01 * numero_02
+division = numero_01 / numero_02
+modulo = numero_01 % numero_02
+potencia = numero_02 ** numero_02
+div_entero = numero_01 // numero_02
 
 print("Operadores Aritméticos")
 
@@ -23,7 +26,7 @@ print("Operadores Aritméticos")
     Se crean dos listas, la primera contiene el titulo de la operacion aritmetica y la segunda el resultado
 """
 # Lista titulos
-nombre_operacion = ["Suma: ", "Resta: ", "Multiplicación: ","División: ", "Modulo: ", "Potencia: ", "División entero" ]
+nombre_operacion = [f"Suma: {numero_01}+{numero_02}= ", f"Resta: {numero_01}-{numero_02}= ", f"Multiplicación: {numero_01}*{numero_02}= ",f"División: {numero_01}+{numero_02}= ", f"Modulo: {numero_01}%{numero_02}= ", f"Potencia: {numero_01}**{numero_02}= ", f"División entero {numero_01}//{numero_02}= " ]
 # Lista resultados
 resultado_operacion = [suma, resta, multiplicacion, division, modulo, potencia, div_entero]
 
