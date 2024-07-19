1f8166b7d0b6b6c28b9327be5afb1563d7f7fa58
Gustavo <greynaga@outlook.com>
2024-07-19 11:11:30 -0700

    se agrega ejercicio 07

---
+++ Ejercicios_15-19_Julio/07-operadores_asignacion_python.py
@@ -0,0 +1,68 @@
+"""
+    Ejemplos de uso de operadores en Python
+    link: https://www.python.org/
+    Creado por: Gustavo Reynaga
+    Julio del 2024
+"""
+
+"""
+    Operadores de Asignación en Python
+"""
+
+print("Operadores de Asignación")
+igual = "Operador '= (igual)': a=5 "
+a=5
+print(igual,a)
+#
+mas_igual = "Operador '+ (mas)': a+=5 "
+a+=5
+print(mas_igual,a)
+#
+menos_igual = "Operador '- (menos)': a-=5 "
+a-=5
+print(menos_igual,a)
+#
+por_igual = "Operador '* (multiplicación)': a*=5 "
+a*=3
+print(por_igual,a)
+#
+div_igual = "Operador '/ (división)': a/=3"
+a/=3
+print(div_igual,a)
+#
+modulo_igual = "Operador '% (modulo)': a%=3 "
+a%=3
+print(modulo_igual,a)
+#
+potencia_igual = "Operador '** (potencia)': a**=3"
+a**=3
+print(potencia_igual,a)
+#
+division_entero = "Operador '// (division entero)': a//=3"
+a//=3
+print(division_entero,a)
+#
+a=3 # Se asigna valor int para hacer operaciones con Bits
+bit_bit_and = "Operador '&= (and) en bits': a&=3"
+a&=3
+print(bit_bit_and,a)
+#
+a=3 # Se asigna valor int para hacer operaciones con Bits
+bit_bit_or = "Operador '|= (or) en bits': a|=3"
+a|=3
+print(bit_bit_or,a)
+#
+a=3 # Se asigna valor int para hacer operaciones con Bits
+bit_bit_xor = "Operador '^= (xor) en bits': a|=3"
+a^=3
+print(bit_bit_xor,a)
+#
+a=3 # Se asigna valor int para hacer operaciones con Bits
+bit_bit_izq = "Operador '>>= (Desplaza bits a la izquierda)': a>>=3"
+a>>=3
+print(bit_bit_izq,a)
+#
+a=3 # Se asigna valor int para hacer operaciones con Bits
+bit_bit_der = "Operador '<<= (Desplaza bits a la derecha)': a<<=3"
+a<<=3
+print(bit_bit_der,a)
