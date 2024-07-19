2bbafa707fb3cb8329ea0176160fb6010cb59815
Gustavo <greynaga@outlook.com>
2024-07-19 12:12:54 -0700

    se agrega ejercicio 08

---
+++ Ejercicios_15-19_Julio/08-operadores_logicos_python.py
@@ -0,0 +1,23 @@
+"""
+    Ejemplos de uso de operadores en Python
+    link: https://www.python.org/
+    Creado por: Gustavo Reynaga
+    Julio del 2024
+"""
+
+print("Operadores Lógicos")
+x = True
+y = False
+print("x=True, y=False")
+operador_and = "Operador 'and' x and y: "
+a= x and y
+print(operador_and,a)
+#
+operador_or = "Operador 'or' x or y: "
+a= x or y
+print(operador_or,a)
+#
+operador_not = "Operador 'not' not x: "
+a= not x
+print(operador_not,a)
+
