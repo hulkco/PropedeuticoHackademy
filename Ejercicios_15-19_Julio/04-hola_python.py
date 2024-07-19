3c42c22f79d059e60715f02a167b8e06c3d25412
Gustavo <greynaga@outlook.com>
2024-07-18 18:48:36 -0700

    se agrega ejercicio 04

---
+++ Ejercicios_15-19_Julio/04-hola_python.py
@@ -0,0 +1,10 @@
+"""
+    Imprimir por terminal un mensaje
+    con Python
+    link: https://www.python.org/
+    Creado por: Gustavo Reynaga
+    Julio del 2024
+"""
+
+lenguaje = "Python"
+print(f"¡Hola, {lenguaje}!")
