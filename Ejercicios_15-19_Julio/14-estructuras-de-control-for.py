a6ef978d11ab21756720f7adeeeb89e7717cc51f
Gustavo <greynaga@outlook.com>
2024-07-19 19:00:17 -0700

    se agrega ejercicio 14

---
+++ Ejercicios_15-19_Julio/14-estructuras-de-control-for.py
@@ -0,0 +1,27 @@
+"""
+    Ejemplos de uso de Estructuras de control en Python
+    link: https://www.python.org/
+    Creado por: Gustavo Reynaga
+    Julio del 2024
+"""
+
+"""
+    Estructuras de control en Python   
+    Ciclo 
+        for
+"""
+print("Estructuras de control en Python")
+print("Ciclo 'for'")
+print("Ejemplo 1: Se usara el ciclo for para imprimir en la\n terminal todos los elementos de una cadena")
+mensaje01 = "HACKADEMY"
+print("Tenemos la cadena:\nmensaje01='HACKADEMY'\n\nfor i in mensaje01:\n     print(i)\n")
+
+for i in mensaje01:
+    print(i)
+print("\nEjemplo 2: Se tiene la siguiente lista\n")
+items =["item1","item2","item3","item4","item5"]
+print('items =["item1","item2","item3","item4","item5"]\n')
+print("Se usara el bucle/ciclo for para recorrer (iterar)\ntodos los elementos de la lista\n")
+print("for i in items:\n    print(i)\n")
+for i in items:
+    print(i)
